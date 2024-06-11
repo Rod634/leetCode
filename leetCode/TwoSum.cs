c1d5a64f95fd9038d0915eebc961e07ca01b7a53
rodrigo silva <rodrigocsilva63@gmail.com>
2024-06-10 21:22:02 -0300

    Two Sum chalenge!

---
+++ leetCode/TwoSum.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        int[] output = new int[2];
+        var alreadySeen = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (alreadySeen.ContainsKey(diff))
+            {
+                output = [alreadySeen.GetValueOrDefault(diff), i];
+                break;
+            }
+            else 
+            {
+                alreadySeen.TryAdd(nums[i], i);
+            }
+            
+        }
+
+        return output;
+    }
+}
