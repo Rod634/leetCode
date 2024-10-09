f04f5bb3181421119dd548b1fbacaff3372644ef
rodrigo silva <rodrigocsilva63@gmail.com>
2024-10-09 09:27:50 -0300

    add comments

--- leetCode/TwoSum/TwoSum.cs
+++ leetCode/TwoSum/TwoSum.cs
@@ -5,11 +5,14 @@
         public int[] Sum(int[] nums, int target)
         {
             int[] output = new int[2];
+            //Dictionary of numbers that already passed through the loop
             var alreadySeen = new Dictionary<int, int>();
 
             for (int i = 0; i < nums.Length; i++)
             {
+                //Subtraction of the number with the target to find the difference value
                 int diff = target - nums[i];
+                //Check if we already seen the diff value
                 if (alreadySeen.ContainsKey(diff))
                 {
                     output = [alreadySeen.GetValueOrDefault(diff), i];
@@ -17,6 +20,7 @@
                 }
                 else
                 {
+                    //Add the number in alreadySeen Dictionary with their index
                     alreadySeen.TryAdd(nums[i], i);
                 }
             }
